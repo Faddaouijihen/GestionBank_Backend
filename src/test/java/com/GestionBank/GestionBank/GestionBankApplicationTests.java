edde465fb7d6974e9dc1f7c911e389ff5ec62ddd
Faddaouijihen <faddaouijihen98@gmail.com>
2024-10-23 21:15:26 +0100

    Initial commit

---
+++ src/test/java/com/GestionBank/GestionBank/GestionBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GestionBank.GestionBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
